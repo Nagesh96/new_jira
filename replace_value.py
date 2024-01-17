b9ec339a980627b564bed5c9dbc2d7997faee0a4
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2024-01-17 19:21:29 +0530

    Update replace_value.py

--- replace_value.py
+++ replace_value.py
@@ -7,8 +7,8 @@ new_value_info = new_values[0].split('-')[0:2]
 # Remove old values with the same artifact ID and version
 current_values = [value for value in current_values if value.split('-')[0:2] != new_value_info]
 
-# Add new value
-current_values.append(new_values[0])
+# Add new value at the beginning
+current_values = [new_values[0]] + current_values
 
 updated_values_str = ",".join(current_values)
 
